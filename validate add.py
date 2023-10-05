58e840f20a748a99a8b2cec4a712c8b3ae99d341
jyothikorrapati123 <144519938+jyothikorrapati123@users.noreply.github.com>
2023-10-05 11:05:52 +0530

    Add files via upload

---
+++ validate add.py
@@ -0,0 +1,25 @@
+hno=(input("enter  hno:"))
+if(hno.isnumeric()):
+    print("address is valid")
+elif(hno.isalpha()):
+    print("address is not valid")
+street = (input("enter  street:"))
+if(street.isalpha()):
+    print("address is valid")
+elif(street.isnumeric()):
+    print("address is not valid")
+vilagename= (input("enter vilagename:"))
+if(vilagename.isalpha()):
+    print("address is valid")
+elif(vilagename.isnumeric()):
+    print("address is not valid")
+state = (input("enter state:"))
+if(state.isalpha()):
+    print("address is valid")
+elif(state.isnumeric()):
+    print("address is not valid")
+dir= (input("enter dir:"))
+if(dir.isalpha()):
+    print("address is valid")
+elif(dir.isnumeric()):
+    print("address is not valid")
